ec5db9985e0c917af2b058c12606feacd55a89c8
hjdtl <873804682@qq.com>
2018-04-20 17:30:23 +0800

    *add axios

--- src/articlelist/ArticleBlock.js
+++ src/articlelist/ArticleBlock.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react'
 import './ArticleBlock.css'
+import axios from 'axios'
 
 class ArticleBlock extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {articleList: []};
+    }
+    
+    
+    componentWillMount(){
+        // axios.get('https://www.easy-mock.com/mock/5ad94f6eb3c0752c6acdcd2d/example/articles')
+        //     .then((res)=>{
+        //         this.setState({articleList:res.data.list})
+        //     })
+        //     .catch((err)=>{
+        //         console.log(err)
+        //     });
+    }
+    
     render() {
         return (
             <div className="article-block">
-                <img src="https://pic2.zhimg.com/80/75d12a08c189cc7bf2ceeeb9779282b4_hd.jpg"/>
+                <img alt="img" src="https://pic2.zhimg.com/80/75d12a08c189cc7bf2ceeeb9779282b4_hd.jpg"/>
                 <div className="info">
                     <div>title</div>
                     <div>content</div>
--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -1,16 +1,37 @@
 import React,{Component} from 'react'
 import './Menu.css'
+import axios from 'axios'
 
 class Menu extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {list: []};
+    }
+    
+    
+    
+    componentWillMount(){
+        axios.get('https://www.easy-mock.com/mock/5ad94f6eb3c0752c6acdcd2d/example/getMenus')
+            .then((res)=>{
+                this.setState({list:res.data.list})
+            })
+            .catch((err)=>{
+                console.log(err)
+            });
+    }
+    
     render(){
         return(
             <div className="menu">
                 <ul>
-                    <li>11111111111111111111111111111111111111111111111111111111</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
-                    <li>5</li>
+                    {
+                        this.state.list.map((item)=>{
+                            return(
+                                <li key={item.text}>
+                                <a href={item.href}>{item.text}</a>
+                                </li>)
+                        })
+                    }
                 </ul>
             </div>
         )
